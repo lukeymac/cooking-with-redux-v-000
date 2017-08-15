8778ac15fc278e00611da9c4b8b5014e4bcaf216
lukeymac <lukey.mac@outlook.com>
2017-08-15 15:20:59 -0400

    Done.

--- rails_auth/app/models/user.rb
+++
@@ -1,6 +0,0 @@
-class User < ApplicationRecord
-  has_secure_password
-  # password_digest
-  # user.authenticate(password)
-    #
-end
--- src/reducers/users.js
+++
@@ -1,9 +0,0 @@
-export default function usersReducer(state = [], action){
-  switch (action.type) {
-    case 'ADDING_USERS':
-      return [...action.payload.users]
-      break;
-    default:
-      return state
-  }
-}
